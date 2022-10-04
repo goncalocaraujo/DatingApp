e7ae69d106c788f7d1a82638c690ae13e9d0577a
goncalocaraujo <garaujo@egapi.pt>
2022-10-04 16:12:47 +0100

    End of section 7

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
@@ -16,8 +16,7 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { SharedModule } from './_modules/shared.module';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { ServerErrorComponent } from './server-error/server-error.component';
+import { ErrorInterceptor } from './_interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -29,9 +28,7 @@ import { ServerErrorComponent } from './server-error/server-error.component';
     MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
-    TestErrorsComponent,
-    NotFoundComponent,
-    ServerErrorComponent
+    TestErrorsComponent
   ],
   imports: [
     BrowserModule,
@@ -41,7 +38,9 @@ import { ServerErrorComponent } from './server-error/server-error.component';
     FormsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
